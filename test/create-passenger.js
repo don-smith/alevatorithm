afcc5eee444861eb6246699f772c2da8f0e4acd4
Don Smith <locksmithdon@gmail.com>
2016-04-17 12:32:59 +1200

    Better checks of source and destination

--- test/create-passenger.js
+++ test/create-passenger.js
@@ -2,7 +2,7 @@ import test from 'ava'
 import 'babel-register'
 import createPassenger from '../lib/create-passenger'
 
-test('create passenger', (t) => {
+test('source and destination are inside building', (t) => {
   let building = {
     elevatorCount: 2,
     numberOfLevels: 13,
@@ -12,10 +12,33 @@ test('create passenger', (t) => {
   let passenger = createPassenger(building)
 
   if (passenger.direction > 0) {
+    // going up
     t.ok(passenger.source <= 0)
-    t.ok(passenger.destination > 0)
+    t.ok(passenger.source >= building.numberOfSublevels * -1)
+    t.ok(passenger.destination >= 0)
+    t.ok(passenger.destination <= building.numberOfLevels)
   } else {
-    t.ok(passenger.source > 0)
+    // going down
+    t.ok(passenger.source >= 0)
+    t.ok(passenger.source <= building.numberOfLevels)
     t.ok(passenger.destination <= 0)
+    t.ok(passenger.destination >= building.numberOfSublevels * -1)
+  }
+})
+
+test('source and destination are different', (t) => {
+  let building = {
+    elevatorCount: 1,
+    numberOfLevels: 2,
+    numberOfSublevels: 2,
+    waitFloors: [0, 0]
+  }
+  let passenger = createPassenger(building)
+
+  let count = 15
+  while (count > 0) {
+    // try a bunch of times since it's random
+    t.ok(passenger.source !== passenger.destination)
+    count--
   }
 })
